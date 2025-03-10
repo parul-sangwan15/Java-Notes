c946212fd939b3eae1a5ff46a0c586e88450de7a
Parul Sangwan15 <parul.sangwan1501@gmail.com>
2025-03-11 02:15:57 +0530

    Taking input in java

---
+++ Notes/InputExp.java
@@ -0,0 +1,58 @@
+package Notes;
+
+import java.io.BufferedReader; // Class imported
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class InputExp {
+    public static void main(String[] args) {
+    
+
+        System.out.println("Enter your input: ");
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bfr = new BufferedReader(in);  // BufferedReader requires an onject of ImputStreamReader class
+
+        try{
+            int num = Integer.parseInt(bfr.readLine());
+        System.out.println(num);
+        bfr.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+
+        /* But bufferedReader is a resource 
+         * so you should utilise it and clear it after its usage
+         * Avoid data leaking
+         * Also, can declare the object in try(''), it is autocloseable for some classes.
+         
+         * This is called "Try with Resources"
+         */
+
+         /* Can also use Scanner 
+
+         System.out.println("Enter a number: ");
+         Scanner sc = new Scanner(System.in);
+         int num1 = sc.nextInt();
+         System.out.println(num1);
+         sc.close();
+
+
+
+         You can use finally{} block to execute something irrespective of an exception
+         Mostly used whenever you want to close a resource.
+
+         try{
+           }
+         catch{
+         }
+         finally{
+         
+         }
+         */
+
+
+
+    }
+}
